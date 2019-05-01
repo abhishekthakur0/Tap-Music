d33aadb6c070577e2f557d0d6f78db2324dc9d89
Abhishek Thakur <34036890+kumarcops@users.noreply.github.com>
2019-05-01 18:09:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+    document.addEventListener('keydown', function(event) {
+      if(event.keyCode == 49) {
+        sounds[0].currentTime = 0;
+        sounds[0].play();
+        createBubble(0);
+      }
+      else if(event.keyCode == 50) {
+        sounds[1].currentTime = 0;
+        sounds[1].play();
+        createBubble(1);
+      }
+      else if(event.keyCode == 51) {
+        sounds[2].currentTime = 0;
+        sounds[2].play();
+        createBubble(2);
+      }
+      else if(event.keyCode == 52) {
+        sounds[3].currentTime = 0;
+        sounds[3].play();
+        createBubble(3);
+      }
+      else if(event.keyCode == 53) {
+        sounds[4].currentTime = 0;
+        sounds[4].play();
+        createBubble(4);
+      }
+      else if(event.keyCode == 54) {
+        sounds[5].currentTime = 0;
+        sounds[5].play();
+        createBubble(5);
+      }
+  });
+
+});
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
